44f78998ee78b4f0c07e67cb2868805a0349bb22
Oliver Northwood <oliver@khanacademy.org>
2016-03-01 22:31:14 -0800

    Add persistent storage (localStorage)

--- index.jsx
+++ index.jsx
@@ -1,12 +1,17 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const { Provider } = require('react-redux');
-const { createStore } = require('redux');
+const { compose, createStore } = require('redux');
+const persistState = require('redux-localstorage');
 
 const weightAppReducers = require('./reducers/index.js');
 const App = require('./app.jsx');
 
-const store = createStore(weightAppReducers);
+const createPersistentStore = compose(
+    persistState()
+)(createStore);
+
+const store = createPersistentStore(weightAppReducers);
 
 ReactDOM.render(
     <Provider store={store}>
