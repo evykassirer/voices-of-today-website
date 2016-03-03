32e0e4448c0ae116ef279c58b5f82d44a7e5f577
Oliver Northwood <oliver@khanacademy.org>
2016-03-02 22:18:17 -0800

    Add redux devtools
    This is the Naive Way™.

--- index.jsx
+++ index.jsx
@@ -4,18 +4,24 @@ const { Provider } = require('react-redux');
 const { compose, createStore } = require('redux');
 const persistState = require('redux-localstorage');
 
+const DevTools = require('./containers/DevTools.jsx');
+
 const weightAppReducers = require('./reducers/index.js');
 const App = require('./app.jsx');
 
 const createPersistentStore = compose(
-    persistState()
+    persistState(),
+    DevTools.instrument()
 )(createStore);
 
 const store = createPersistentStore(weightAppReducers);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <div>
+            <App />
+            <DevTools />
+        </div>
     </Provider>,
     document.getElementById('content')
 );
