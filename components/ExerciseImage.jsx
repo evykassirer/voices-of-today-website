e19e74d7b69f22649b2c8e5eb1e9c7351dec571a
Oliver Northwood <oliver@khanacademy.org>
2016-03-18 16:57:22 -0400

    Move colors to a styles file

--- components/ExerciseImage.jsx
+++ components/ExerciseImage.jsx
@@ -1,63 +1,8 @@
 const React = require('react');
 const { css, StyleSheet } = require('../lib/aphrodite.js');
 
-
-const domainColors = {
-    'lightBlue': {
-        light: "#11accd",
-        lightest: "#63d9ea",
-        dark: "#007d96",
-        darkest: "#085566",
-    },
-    'teal': {
-        light: "#01a995",
-        lightest: "#01d1c1",
-        dark: "#208170",
-        darkest: "#144f44",
-    },
-    'green': {
-        light: "#1fab54",
-        lightest: "#74cf70",
-        dark: "#0d923f",
-        darkest: "#085e29",
-    },
-    'yellow': {
-        light: "#e07d10",
-        lightest: "#ffbe26",
-        dark: "#a75a05",
-        darkest: "#953c02",
-    },
-    'red': {
-        light: "#e84d39",
-        lightest: "#ff8482",
-        dark: "#be2612",
-        darkest: "#8c1c0d",
-    },
-    'pink': {
-        light: "#ca337c",
-        lightest: "#ff92c6",
-        dark: "#9e034e",
-        darkest: "#6b0235",
-    },
-    'purple': {
-        light: "#7853ab",
-        lightest: "#aa87ff",
-        dark: "#543b78",
-        darkest: "#302245",
-    },
-    'blue': {
-        light: "#134ea3",
-        lightest: "#66afe9",
-        dark: "#00457c",
-        darkest: "#002a4b",
-    },
-    'grey': {
-        light: "#cccccc",
-        lightest: "#eeeeee",
-        dark: "#888888",
-        darkest: "#444444",
-    },
-};
+const SS = require('../styles.js');
+const colors = SS.colors;
 
 const exerciseOptions = [
     'dumbbell shoulder press',
@@ -114,9 +59,9 @@ const ExerciseImage = React.createClass({
     },
 
     renderExerciseImage: function(ex) {
-        const personColor = domainColors[this.props.color].darkest;
-        const weightColor = domainColors[this.props.color].dark;
-        const chairColor = domainColors[this.props.color].light;
+        const personColor = colors[this.props.color].darkest;
+        const weightColor = colors[this.props.color].dark;
+        const chairColor = colors[this.props.color].light;
 
         const exerciseImages = {
             'dumbbell shoulder press': [
@@ -464,7 +409,7 @@ const ExerciseImage = React.createClass({
         const style = {
             height: this.props.height,
             width: this.props.height * 100 / 60,
-            backgroundColor: domainColors[this.props.color].lightest,
+            backgroundColor: colors[this.props.color].lightest,
         };
 
         return (
---
+++ styles.js
@@ -0,0 +1,60 @@
+const colors = {
+    'lightBlue': {
+        light: "#11accd",
+        lightest: "#63d9ea",
+        dark: "#007d96",
+        darkest: "#085566",
+    },
+    'teal': {
+        light: "#01a995",
+        lightest: "#01d1c1",
+        dark: "#208170",
+        darkest: "#144f44",
+    },
+    'green': {
+        light: "#1fab54",
+        lightest: "#74cf70",
+        dark: "#0d923f",
+        darkest: "#085e29",
+    },
+    'yellow': {
+        light: "#e07d10",
+        lightest: "#ffbe26",
+        dark: "#a75a05",
+        darkest: "#953c02",
+    },
+    'red': {
+        light: "#e84d39",
+        lightest: "#ff8482",
+        dark: "#be2612",
+        darkest: "#8c1c0d",
+    },
+    'pink': {
+        light: "#ca337c",
+        lightest: "#ff92c6",
+        dark: "#9e034e",
+        darkest: "#6b0235",
+    },
+    'purple': {
+        light: "#7853ab",
+        lightest: "#aa87ff",
+        dark: "#543b78",
+        darkest: "#302245",
+    },
+    'blue': {
+        light: "#134ea3",
+        lightest: "#66afe9",
+        dark: "#00457c",
+        darkest: "#002a4b",
+    },
+    'grey': {
+        light: "#cccccc",
+        lightest: "#eeeeee",
+        dark: "#888888",
+        darkest: "#444444",
+    },
+};
+
+module.exports = {
+    colors
+};
