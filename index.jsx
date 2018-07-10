ebbbe8f3d9df2532b46be8b1169aaecd964c036f
DESKTOP-1GG9SCC\Celeste <clairross359@gmail.com>
2018-07-10 15:30:16 -0700

    added accessibility changes

--- index.jsx
+++ index.jsx
@@ -9,6 +9,7 @@ const Volunteer = require('./components/volunteer.jsx');
 const Schedule = require('./components/schedule.jsx');
 const Schedule2017 = require('./components/schedule2017.jsx');
 const Mandate = require('./components/mandate.jsx');
+const Accessibility = require('./components/accessibility.jsx');
 
 ReactDOM.render((
   <Router history={browserHistory}>
@@ -17,6 +18,7 @@ ReactDOM.render((
     <Route name="schedule" path="/schedule" component={Schedule}/>
     <Route name="schedule2017" path="/schedule2017" component={Schedule2017}/>
     <Route name="mandate" path="/mandate" component={Mandate}/>
+    <Route name="accessibility" path="/accessibility" component={Accessibility}/>
 
     <Route name="default" path="*" component={HomePage}/>
   </Router>
